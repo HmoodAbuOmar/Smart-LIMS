2f282ffb83cf59e68294ca77cb0a72203250914f
HmoodAbuOmar <hmoodabuomar2018@gmail.com>
2026-09-23 23:00:51 +0300

    Initialize Spring Boot backend

---
+++ Backend/smart-lims/src/test/java/com/smartlims/SmartLimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
